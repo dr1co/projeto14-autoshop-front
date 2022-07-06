3949dbf75fddecfa876522e870f0327e5a34a23a
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-07-06 10:20:22 -0300

    fix: axios added & signup route added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserContextProvider from './contexts/UserContext';
+import SignUp from './pages/SignUp';
 
 export default function App() {
     return (
         <BrowserRouter>
             <UserContextProvider>
                 <Routes>
-                    Inserir rotas aqui.
+                    <Route path={"/signup"} element={<SignUp/>}/>
                 </Routes>
             </UserContextProvider>
         </BrowserRouter>
