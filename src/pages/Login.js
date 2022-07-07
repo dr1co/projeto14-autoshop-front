c4593cc7d76f7e8b3aa78f6fd2ab88a1698f1037
dr1co <dricomatoba@hotmail.com>
2022-07-07 12:28:01 -0300

    changed features to match current layout

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Form from "../components/shared/Form.js";
 import Input from "../components/shared/Input.js";
 import ButtonForm from "../components/shared/ButtonForm.js";
+import { FlexContainer } from "../components/styles/FlexContainer";
 import { useUserContext } from '../contexts/UserContext';
 
 export default function Login (){
@@ -36,12 +38,29 @@ export default function Login (){
     }
 
     return(
-        <>
+        <FlexContainer direction={"column"} justify={"center"} align={"center"}>
+            <Title> Faça login </Title>
             <Form endpoint={"/login"} action={validate} body={body}>
                 <Input text={"Email"} name={"email"} setValue={setEmail} value={email} result={result} />
                 <Input text={"Senha"} setValue={setPassword} value={password} type={"password"} />
                 <ButtonForm text={"Fazer login"} isDisabled={isDisabled} />
             </Form>
-        </>
+            <StyledSpan onClick={() => navigate("/signup")}> Não possui uma conta? Cadastre-se! </StyledSpan>
+        </FlexContainer>
     );
 }
+
+const Title = styled.span`
+    font-size: 22px;
+    font-weight: 600;
+    color: black;
+    margin-bottom: 20px;
+`;
+
+const StyledSpan = styled.span`
+    margin-top: 20px;
+    font-size: 16px;
+    font-weight: 600;
+    color:black;
+    cursor: pointer;
+`;
