edae8488c1ea70f6c1e0e62bbcc92f7b6061b8ce
dr1co <dricomatoba@hotmail.com>
2022-07-09 11:10:18 -0300

    added basic header components

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -4,17 +4,18 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useUserContext } from '../../contexts/UserContext';
-import { IonSearchbar } from '@ionic/react';
+import { IonSearchbar, IonIcon } from '@ionic/react';
+import { menuOutline, cart } from 'ionicons/icons';
 
 export default function Header() {
     return (
         <Container>
             <Menu>
-
+                <IonIcon icon={menuOutline} />
             </Menu>
             <IonSearchbar />
             <Cart>
-
+                <IonIcon icon={cart} />
             </Cart>
         </Container>
     )
@@ -31,4 +32,14 @@ const Container = styled.div`
     position: fixed;
     top: 0;
     left: 0;
+`;
+
+const Menu = styled.div`
+    width: 100%;
+    height: fit-content;
+`;
+
+const Cart = styled.div`
+    width: 100%;
+    height: fit-content;
 `;
